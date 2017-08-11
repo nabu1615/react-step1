dc7be09e6710b7adb81a41f7ac51a74b29258fbc
Javier Vargas <javier.vargas@globant.com>
2017-08-11 16:08:29 -0500

    change copy texts

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render () {
     return (
       <div>
-        <h1>Hola Mundo</h1>
-        <p> Soy un Parrafo </p>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
